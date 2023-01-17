73b1b7a8a13b9f583d9796614fce033dad0eabe2
Alexander Schaub <alexander.schaub@helixton.de>
2023-01-17 11:27:54 +0100

    new: list all files of directory
    - added walkdir
    - added loop for all_files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
-use helixton_toolset;
+// use helixton_toolset;
+
+use std::path::Path;
+use std::process::exit;
+
+extern crate walkdir;
+use walkdir::WalkDir;
+
+fn abort(msg: &str) {
+    println!("{}", msg);
+    exit(-1);
+}
 
 fn main() {
     println!("Hello, world! - from cli");
 
-    helixton_toolset::hello();
+    // TODO: integrate path as argument
+    let target_folder: &Path = Path::new("target/data");
+
+    if !target_folder.exists() && !target_folder.is_dir() {
+        abort("folder does not exists - abort");
+    }
+
+    let all_files = WalkDir::new(target_folder)
+        .into_iter()
+        .filter_map(|file| file.ok());
+
+    for file in all_files {
+        println!("file: {}", file.path().display());
+    }
 }

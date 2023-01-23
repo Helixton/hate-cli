12df72574fe8dc4893ce087e3ef6b6bbdad4a4cc
Alexander Schaub <alexander.schaub@helixton.de>
2023-01-23 09:42:38 +0100

    new: integrated functions to library
    functions are integrated in helixton-toolset library now

--- lib/helixton-toolset/src/lib.rs
+++ lib/helixton-toolset/src/lib.rs
@@ -1,3 +1,54 @@
+
+use std::process::exit;
+use std::fs;
+use std::path::Path;
+use std::collections::HashMap;
+use sha2::{Sha256, Digest};
+
+extern crate walkdir;
+use walkdir::{WalkDir, DirEntry};
+
 pub fn hello() {
     println!("Hello, world! - from lib");
+}
+
+pub fn abort(msg: &str) {
+    println!("{}", msg);
+    exit(-1);
+}
+
+pub fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
+pub fn get_all_files(target_folder: &Path) -> impl Iterator<Item = DirEntry> {
+    WalkDir::new(target_folder)
+        .into_iter()
+        .filter_map(|file| file.ok())
+}
+
+pub fn get_file_sha256_map(target_folder: &Path) -> HashMap<String, Vec<u8>> {
+    let all_files = get_all_files(target_folder);
+
+    let mut file_hash_map: HashMap<String, Vec<u8>> = HashMap::new();
+
+    for file in all_files {
+
+        let file_path = file.path();
+
+        if !file_path.is_file() {
+            continue;
+        }
+
+        let file_content = fs::read(file_path).unwrap();
+
+        let mut hasher = Sha256::new();
+        hasher.update(file_content);
+        let file_hash = hasher.finalize();
+
+        file_hash_map.insert(file_path.display().to_string(), file_hash.to_vec());
+        
+    }
+
+    file_hash_map
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,17 @@
-// use helixton_toolset;
-
 use std::path::Path;
-use std::process::exit;
 
-extern crate walkdir;
-use walkdir::WalkDir;
-
-fn abort(msg: &str) {
-    println!("{}", msg);
-    exit(-1);
-}
+use helixton_toolset;
 
 fn main() {
-    println!("Hello, world! - from cli");
-
     // TODO: integrate path as argument
     let target_folder: &Path = Path::new("target/data");
-
+    
     if !target_folder.exists() && !target_folder.is_dir() {
-        abort("folder does not exists - abort");
+        helixton_toolset::abort("folder does not exists - abort");
     }
 
-    let all_files = WalkDir::new(target_folder)
-        .into_iter()
-        .filter_map(|file| file.ok());
+    // let all_files = helixton_toolset::get_all_files(&target_folder);
 
-    for file in all_files {
-        println!("file: {}", file.path().display());
-    }
+    let file_hash_map = helixton_toolset::get_file_sha256_map(&target_folder);
+    println!("{:?}", file_hash_map);
 }

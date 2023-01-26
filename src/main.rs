e09839c4968e634ddaaf1c4884fe829d89cd390c
Alexander Schaub <alexander.schaub@helixton.de>
2023-01-26 10:25:23 +0100

    new: renamed project

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use std::path::Path;
 
-use helixton_toolset;
+use hate;
 
 fn main() {
     // TODO: integrate path as argument
     let target_folder: &Path = Path::new("target/data");
     
     if !target_folder.exists() && !target_folder.is_dir() {
-        helixton_toolset::abort("folder does not exists - abort");
+        hate::abort("folder does not exists - abort");
     }
 
-    // let all_files = helixton_toolset::get_all_files(&target_folder);
+    // let all_files = hate::get_all_files(&target_folder);
 
-    let file_hash_map = helixton_toolset::get_file_sha256_map(&target_folder);
+    let file_hash_map = hate::get_file_sha256_map(&target_folder);
     println!("{:?}", file_hash_map);
 }

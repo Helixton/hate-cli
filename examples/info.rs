2714c4a63830d7d996dc914a0961ee5dee67be30
Alexander Schaub <alexander.schaub@helixton.de>
2023-03-20 16:15:31 +0100

    info via serde-json

--- examples/info.rs
+++ examples/info.rs
@@ -1,4 +1,27 @@
 
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize)]
+struct Address {
+    street: String,
+    city: String,
+}
+
+fn print_an_address() -> Result<()> {
+    let address = Address {
+        street: "10 Downing Street".to_owned(),
+        city: "London".to_owned(),
+    };
+
+    let j = serde_json::to_string(&address)?;
+
+    println!("{}", j);
+
+    Ok(())
+}
+
 fn main() {
-    println!("here comes info");
+    
+    print_an_address();
 }
